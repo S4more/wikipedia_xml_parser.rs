8bbe17d2952f8758c0af0845588abc32c116be17
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-10 12:06:00 -0500

    Changed parser method

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,101 @@
-use std::collections::HashMap;
-use std::io::Write;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::{Arc, Mutex};
-use std::time::{Duration, Instant};
-use std::{env, thread};
-use std::fs::{self, File};
+use std::fs::File;
+
+use std::io::BufReader;
+use std::{env::args, time::Instant};
+
 use once_cell::sync::Lazy;
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
 use regex::Regex;
-use threadpool::ThreadPool;
 
-static CONTENT: Lazy<String> = Lazy::new(|| {
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let contents = fs::read_to_string(file_path).expect("File not found. Pass the file name with cargo run -- file.txt.");
-    contents
-});
+static TAG_MATCHER: Lazy<Regex> = Lazy::new(|| Regex::new(r"\[\[(.+?)]]|\|").unwrap());
 
-static TAG_MATCHER: Lazy<Regex> = Lazy::new(|| {
-    let re = Regex::new(r"\[\[.+?(]|\|)").unwrap();
-    re
-});
+// fn parse_content(content:)
 
-static TITLE_MATCHER: Lazy<Regex> = Lazy::new(|| {
-    let get_page_regex = Regex::new(r"<title>[\s\S]*?</title>").unwrap();
-    get_page_regex
-});
+fn get_given_file_reader() -> Option<BufReader<File>> {
+    let arg: Vec<String> = args().collect();
+    if let Some(path) = arg.get(1) {
+        if let Ok(file) = File::open(path) {
+            return Some(BufReader::with_capacity(8096, file));
+        }
+    }
+    None
+}
+
+fn consume_parser(mut parser: Reader<BufReader<File>>) {
+    let mut buf = vec![];
+    let page = String::from("page");
+    let title = String::from("text");
+
+    let mut is_page = false;
+    let mut is_title = false;
+    let mut titles: u64 = 0;
+
+    loop {
+        if buf.len() > 8096 {
+            buf.clear()
+        }
+
+        match (parser.read_event_into(&mut buf), is_page, is_title) {
+            (Ok(Event::Start(content)), false, false) => {
+                match String::from_utf8(content.name().0.to_vec()).unwrap() == page {
+                    true => is_page = true,
+                    _ => {}
+                }
+            }
+
+            (Ok(Event::Start(content)), true, false) => {
+                match String::from_utf8(content.name().0.to_vec()).unwrap() == title {
+                    true => is_title = true,
+                    _ => {}
+                }
+            }
+
+            (Ok(Event::End(content)), true, false) => {
+                match String::from_utf8(content.name().0.to_vec()).unwrap() == page {
+                    true => is_page = false,
+                    _ => {}
+                }
+            }
+
+            (Ok(Event::End(content)), true, true) => {
+                match String::from_utf8(content.name().0.to_vec()).unwrap() == title {
+                    true => is_title = false,
+                    _ => {}
+                }
+            }
+
+            (Ok(Event::Text(content)), true, true) => {
+                if let Some(matches) = TAG_MATCHER.captures(core::str::from_utf8(&content).unwrap())
+                {
+                    if let Some(text) = matches.get(1) {
+                        titles += 1;
+                        if titles % 100 == 0 {
+                            println!("{}", titles);
+                        }
+                        // println!("{}", text.as_str());
+                    }
+                }
+            }
+
+            (Ok(Event::Eof), _, _) => {
+                println!("{:?}", buf.capacity());
+                break;
+            }
+            _ => {}
+        }
+    }
+}
 
 fn main() {
     let start = Instant::now();
-    let n_workers = 32;
-    let pool = ThreadPool::new(n_workers);
+    let reader = get_given_file_reader().expect("Failed to get file");
 
-    // Divide the page into smaller sub-sections so we can multi thread it
-    let get_page_regex = Arc::new(Regex::new(r"<page>[\s\S]*?</page>").unwrap());
+    // let parser = quick_xml::Reader::
+    let parser = Reader::from_reader(reader);
 
-    let it = get_page_regex.captures_iter(&CONTENT);
-    let pages: Arc<Mutex<HashMap<String, Vec<String>>>> = Arc::new(Mutex::new(HashMap::new()));
+    consume_parser(parser);
 
-
-    for page in it {
-        let clone = pages.clone();
-        pool.execute(move || {
-            let tags = parse_page(&page[0].to_string());
-            clone.lock().unwrap().insert(get_page_title(&page[0].to_string()), tags);
-        });
-    }
-
-    pool.join();
-
-    let complete_page = pages.lock().unwrap();
-
-    let mut stats = vec![];
-    for page in complete_page.iter() {
-        stats.push(format!("Page: {} - tags: {} ", page.0, page.1.len()));
-    }
-    //
-    let mut file = File::create("out_parsed_two.txt").unwrap();
-    let json_string = serde_json::to_string(&complete_page.to_owned()).unwrap();
-    file.write(json_string.as_bytes()).unwrap();
-
-    let mut stats_file = File::create("stats.txt").unwrap();
-    stats_file.write(stats.join("\n").as_bytes()).unwrap();
     let duration = start.elapsed();
-    println!("Time elapsed is: {:?}", duration);
-}
-
-fn get_page_title(content: &String) -> String {
-    let page_tag_range = &TITLE_MATCHER.find(&content).unwrap();
-    return content[page_tag_range.start() .. page_tag_range.end()].to_string();
-}
-
-fn parse_page(content: &String) -> Vec<String> {
-    let mut matches: Vec<String> = vec![];
-    let it = TAG_MATCHER.captures_iter(&content);
-
-    for cap in it {
-        let size = cap[0].len();
-        let trimmed = &cap[0][2..size-1];
-        if trimmed.starts_with("File: ") {
-            continue;
-        }
-
-        matches.push(trimmed.to_string());
-    }
-
-    matches
+    println!("Took : {duration:?}");
 }
