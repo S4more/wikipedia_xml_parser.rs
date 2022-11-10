61b87f517d2304a44383f568b53cfc4fd79b6657
S4more <guimsc@hotmail.com>
2022-11-09 22:28:52 -0500

    Use thread pool.

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,51 @@ use std::collections::HashMap;
 use std::io::Write;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::{env, thread};
 use std::fs::{self, File};
 use once_cell::sync::Lazy;
 use regex::Regex;
+use threadpool::ThreadPool;
 
-static CONTENT: Lazy<Arc<String>> = Lazy::new(|| {
+static CONTENT: Lazy<String> = Lazy::new(|| {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let contents = fs::read_to_string(file_path).expect("File not found. Pass the file name with cargo run -- file.txt.");
-    Arc::new(contents)
+    contents
 });
 
-static GLOBAL_THREAD_COUNT: AtomicUsize = AtomicUsize::new(0);
+static TAG_MATCHER: Lazy<Regex> = Lazy::new(|| {
+    let re = Regex::new(r"\[\[.+?(]|\|)").unwrap();
+    re
+});
+
+static TITLE_MATCHER: Lazy<Regex> = Lazy::new(|| {
+    let get_page_regex = Regex::new(r"<title>[\s\S]*?</title>").unwrap();
+    get_page_regex
+});
 
 fn main() {
+    let start = Instant::now();
+    let n_workers = 32;
+    let pool = ThreadPool::new(n_workers);
+
     // Divide the page into smaller sub-sections so we can multi thread it
     let get_page_regex = Arc::new(Regex::new(r"<page>[\s\S]*?</page>").unwrap());
 
     let it = get_page_regex.captures_iter(&CONTENT);
     let pages: Arc<Mutex<HashMap<String, Vec<String>>>> = Arc::new(Mutex::new(HashMap::new()));
 
-    println!("Starting threads.");
 
     for page in it {
         let clone = pages.clone();
-        thread::spawn(move || {
-            GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
+        pool.execute(move || {
             let tags = parse_page(&page[0].to_string());
             clone.lock().unwrap().insert(get_page_title(&page[0].to_string()), tags);
-            GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
         });
     }
 
-
-    while GLOBAL_THREAD_COUNT.load(Ordering::SeqCst) != 0 {
-        println!("Waiting... {} threads still running", GLOBAL_THREAD_COUNT.load(Ordering::SeqCst));
-        thread::sleep(Duration::from_millis(1)); 
-    }
+    pool.join();
 
     let complete_page = pages.lock().unwrap();
 
@@ -55,20 +61,18 @@ fn main() {
 
     let mut stats_file = File::create("stats.txt").unwrap();
     stats_file.write(stats.join("\n").as_bytes()).unwrap();
+    let duration = start.elapsed();
+    println!("Time elapsed is: {:?}", duration);
 }
 
 fn get_page_title(content: &String) -> String {
-    let get_page_regex = Regex::new(r"<title>[\s\S]*?</title>").unwrap();
-    let page_tag = &get_page_regex.captures_iter(&content).into_iter().next().unwrap()[0];
-
-    // println!("{}", page_tag);
-    return page_tag.to_string();
+    let page_tag_range = &TITLE_MATCHER.find(&content).unwrap();
+    return content[page_tag_range.start() .. page_tag_range.end()].to_string();
 }
 
 fn parse_page(content: &String) -> Vec<String> {
-    let re = Regex::new(r"\[\[.+?(]|\|)").unwrap();
     let mut matches: Vec<String> = vec![];
-    let it = re.captures_iter(&content);
+    let it = TAG_MATCHER.captures_iter(&content);
 
     for cap in it {
         let size = cap[0].len();
